73570e050c9dc643780f23f939dea90fd73222c7
seblewonge <seblewongelrcd@gmail.com>
2025-10-17 00:48:15 +0300

    Add Netlify redirects for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "./", // important for Netlify or GitHub Pages
-//   build: {
-//     outDir: "dist"
-//   }
-// });
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: '_redirects', dest: '' }
-      ]
-    })
-  ],
+  plugins: [react()],
+  base: "./", // important for Netlify or GitHub Pages
+  build: {
+    outDir: "dist"
+  }
 });
